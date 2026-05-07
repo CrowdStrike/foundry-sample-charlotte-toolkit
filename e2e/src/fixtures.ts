16dee49d28567fab1b27cd63f4db7761851b492b
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 14:22:41 -0600

    Remove unused fixtures from e2e tests
    Only charlotteExtensionPage is used in the test suite. Removed AppCatalogPage, appName, and config fixtures that were defined but never destructured.

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,27 +1,13 @@
 import { test as baseTest } from '@playwright/test';
-import {
-  AppCatalogPage,
-  config,
-} from '@crowdstrike/foundry-playwright';
 import { CharlotteExtensionPage } from './pages/CharlotteExtensionPage';
 
 type FoundryFixtures = {
-  appCatalogPage: AppCatalogPage;
   charlotteExtensionPage: CharlotteExtensionPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
   charlotteExtensionPage: async ({ page }, use) => {
     await use(new CharlotteExtensionPage(page));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
   },
 });
 
